28f791a52e49be71dc5a65a3726bbd11bd6b926b
defibulls <defibulls@gmail.com>
2022-10-23 20:26:15 +0530

    complete phase 2

--- components/support/FAQs.tsx
+++ components/support/FAQs.tsx
@@ -25,6 +25,7 @@ const faqs = [
 ]
 
 const FAQs = (props: Props) => {
+  const [show, handleShow] = useState(false)
   return (
     <div
       id="faqs"
@@ -41,18 +42,28 @@ const FAQs = (props: Props) => {
           />
         </div>
       </div>
-      <div className="grid grid-cols-1 gap-10 transition-all duration-1000 ease-out md:grid-cols-2">
+      <div className="grid grid-cols-1 gap-10 transition-all duration-700 md:grid-cols-2">
         {faqs.map((faq, i) => (
           <div
             key={i}
-            className="group flex h-fit flex-col rounded-md bg-gray-800 px-20 py-8 transition duration-500 ease-in-out"
+            className="group flex h-fit flex-col rounded-md bg-gray-800 px-20 py-8 transition-all duration-500 ease-in-out"
           >
-            <div className="flex w-full items-center justify-between">
+            <div className="flex  items-center justify-between">
               <h1 className="font-bold uppercase tracking-widest text-gray-400">
                 {faq.question}
               </h1>
+              {/* <div
+                className="cursor-pointer rounded-md bg-gray-600 p-2"
+                onClick={() => handleShow(!show)}
+              >
+                {show ? (
+                  <MinusIcon className="h-4" />
+                ) : (
+                  <PlusIcon className="h-4" />
+                )}
+              </div> */}
             </div>
-            <div className="mt-6 hidden group-hover:block">
+            <div className={`mt-6 hidden group-hover:block`}>
               <p className="text-xs font-normal tracking-widest text-gray-400">
                 {faq.answer}
               </p>
