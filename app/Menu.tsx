a3598ca3797c3e0cc1e99b16bfa541e3137638d7
Sameem <ssssameembaba@gmail.com>
2023-03-18 19:12:05 +0530

    remove openea tab

--- app/Menu.tsx
+++ app/Menu.tsx
@@ -74,9 +74,7 @@ const Menu = (props: Props) => {
           >
             Discord
           </a>
-          <a href="/" target="_blank">
-            OpenSea
-          </a>
+          abc
         </div>
       </div>
       <div>
